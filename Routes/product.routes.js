95287c1af6b8adba2bd2cb55f7a2ebbf6638b543
MSaifKhan01 <msaifkhan5038@gmail.com>
2023-09-22 15:52:59 +0530

    product update and delete

--- Routes/product.routes.js
+++ Routes/product.routes.js
@@ -1,10 +1,13 @@
 const express=require("express")
-const {getAllProducts,getProductDetail,addProduct}=require("../Controllers/product.controller")
+const {getAllProducts,getProductDetail,addProduct, getProductsByCategory, updateProduct, deleteProduct}=require("../Controllers/product.controller")
 
 
 const productRouter=express.Router()
 productRouter.post("/add",addProduct)
 productRouter.get("/get",getAllProducts)
 productRouter.get("/get/:productID",getProductDetail)
+productRouter.get("/category/:categoryID",getProductsByCategory)
+productRouter.put("/update/:productID",updateProduct)
+productRouter.delete("/delete/:productID",deleteProduct)
 
 module.exports={productRouter}
